be58f0b827656efad90fc15b34b5f354ed63155e
Nik Reiman <nik.reiman@gmail.com>
2012-05-18 23:23:19 +0200

    Set company name

--- DJToolbox/AppDelegate.h
+++ DJToolbox/AppDelegate.h
@@ -3,12 +3,13 @@
 //  DJToolbox
 //
 //  Created by Nik Reiman on 5/18/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
+#import "LibraryLocationProvider.h"
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate, LibraryLocationProvider>
 
 @property (assign) IBOutlet NSWindow *window;
 
--- DJToolbox/AppDelegate.m
+++ DJToolbox/AppDelegate.m
@@ -3,7 +3,7 @@
 //  DJToolbox
 //
 //  Created by Nik Reiman on 5/18/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Teragon Audio. All rights reserved.
 //
 
 #import "AppDelegate.h"
