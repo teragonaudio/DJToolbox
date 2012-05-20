7f640da4a444e538ff9ff22c0e29cbc4b7b03eee
Nik Reiman <nik.reiman@gmail.com>
2012-05-20 15:54:39 +0200

    Add orphaned ASD browser controller

---
+++ DJToolbox/OrphanedAsdsController.h
@@ -0,0 +1,12 @@
+//
+// Created by nik on 5/20/12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface OrphanedAsdsController : NSObject
+@end
---
+++ DJToolbox/OrphanedAsdsController.m
@@ -0,0 +1,15 @@
+//
+// Created by nik on 5/20/12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "OrphanedAsdsController.h"
+
+
+@implementation OrphanedAsdsController {
+
+}
+
+@end
