30eb23b1e59a4d8e31edb08731f2359e4ad1c1e7
Nik Reiman <nik.reiman@gmail.com>
2012-05-20 10:42:32 +0200

    Implement unwarped files feature
    Also add referencing outlets for all buttons, as they must be
    activated/deactivated depending on the app's state.

--- DJToolbox/AppDelegate.h
+++ DJToolbox/AppDelegate.h
@@ -11,11 +11,16 @@
 
 static NSString *const kDefaultsKeyPastLibraryLocations = @"Past Library Locations";
 static NSString *const kDefaultsKeyLastLibraryLocation = @"Last Library Location";
+static NSString *const kDefaultsKeyLastUnwarpedTracksOutputFolder = @"Last Unwarped Tracks Output Folder";
+
+static NSString *const kDefaultOutputPlaylistName = @"/unwarped.m3u8";
+static NSString *const kiTunesTopLevelSubfolder = @"/iTunes Media/Music";
 
 @interface AppDelegate : NSObject <NSApplicationDelegate, LibraryLocationProvider>
 
 #pragma mark Internal Members
 @property (assign, nonatomic) NSString *currentLibraryLocation;
+@property (readonly) NSArray *recognizedAudioFileExtensions;
 
 #pragma mark Main Window Controls
 @property (assign) IBOutlet NSWindow *window;
@@ -26,21 +31,29 @@ static NSString *const kDefaultsKeyLastLibraryLocation = @"Last Library Location
 
 #pragma mark Ableton Live Tab
 @property (assign) IBOutlet NSTextField *unwarpedTracksOutputFolderTextField;
+@property (assign) IBOutlet NSButton *unwarpedTracksGeneratePlaylistButton;
 @property (assign) IBOutlet NSBrowser *orphanedAsdsBrowser;
+@property (assign) IBOutlet NSButton *findOrphanedAsdsButton;
 - (IBAction)browseUnwarpedTracksOutputFolder:(id)sender;
 - (IBAction)generateUnwarpedTracksPlaylist:(id)sender;
 - (IBAction)findOrphanedAsds:(id)sender;
 
 #pragma mark Dropbox Tab
 @property (assign) IBOutlet NSBrowser *conflictedFilesBrowser;
+@property (assign) IBOutlet NSButton *findConflictedFilesButton;
+@property (assign) IBOutlet NSButton *conflictedFilesUseMineButton;
+@property (assign) IBOutlet NSButton *conflictedFilesUseTheirsButton;
 - (IBAction)findConflictedFiles:(id)sender;
 - (IBAction)useMineForConflictedFile:(id)sender;
 - (IBAction)useTheirsForConflictedFile:(id)sender;
 
 #pragma mark Finder Tab
+@property (assign) IBOutlet NSButton *cleanJunkFilesButton;
 - (IBAction)cleanJunkFiles:(id)sender;
 
 #pragma mark iTunes Tab
+@property (assign) IBOutlet NSButton *findOrphanedTracksButton;
+@property (assign) IBOutlet NSButton *addOrphanedTracksToLibraryButton;
 @property (assign) IBOutlet NSBrowser *orphanedTracksBrowser;
 - (IBAction)findOrphanedFiles:(id)sender;
 - (IBAction)addOrphanedTracksToLibrary:(id)sender;
--- DJToolbox/AppDelegate.m
+++ DJToolbox/AppDelegate.m
@@ -11,31 +11,66 @@
 @implementation AppDelegate
 
 @synthesize currentLibraryLocation = _currentLibraryLocation;
+
 @synthesize window = _window;
 @synthesize libraryComboBox = _libraryComboBox;
 @synthesize statusTextField = _statusTextField;
 @synthesize progressIndicator = _progressIndicator;
+
 @synthesize unwarpedTracksOutputFolderTextField = _unwarpedTracksOutputFolderTextField;
+@synthesize unwarpedTracksGeneratePlaylistButton = _unwarpedTracksGeneratePlaylistButton;
 @synthesize orphanedAsdsBrowser = _orphanedAsdsBrowser;
+@synthesize findOrphanedAsdsButton = _findOrphanedAsdsButton;
+
 @synthesize conflictedFilesBrowser = _conflictedFilesBrowser;
+@synthesize findConflictedFilesButton = _findConflictedFilesButton;
+@synthesize conflictedFilesUseMineButton = _conflictedFilesUseMineButton;
+@synthesize conflictedFilesUseTheirsButton = _conflictedFilesUseTheirsButton;
+
+@synthesize cleanJunkFilesButton = _cleanJunkFilesButton;
+
+@synthesize findOrphanedTracksButton = _findOrphanedTracksButton;
+@synthesize addOrphanedTracksToLibraryButton = _addOrphanedTracksToLibraryButton;
 @synthesize orphanedTracksBrowser = _orphanedTracksBrowser;
 
+
+- (NSString *)getiTunesLibraryLocation {
+  return self.currentLibraryLocation;
+}
+
+- (NSArray *)recognizedAudioFileExtensions {
+  return [NSArray arrayWithObjects:@"mp3", @"m4a", @"flac", @"ogg", nil];
+}
+
+- (NSString *)lastUnwarpedTracksOutputFolder {
+  NSString *outputPath = [[NSUserDefaults standardUserDefaults] stringForKey:kDefaultsKeyLastUnwarpedTracksOutputFolder];
+  if(outputPath == nil) {
+    return [NSHomeDirectory() stringByAppendingString:@"/Desktop"];
+  }
+  else {
+    return outputPath;
+  }
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
   NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
   NSArray *pastLibraryLocations = [userDefaults arrayForKey:kDefaultsKeyPastLibraryLocations];
   [self.libraryComboBox addItemsWithObjectValues:pastLibraryLocations];
-  [self.libraryComboBox setStringValue:[userDefaults objectForKey:kDefaultsKeyLastLibraryLocation]];
+  self.currentLibraryLocation = [userDefaults objectForKey:kDefaultsKeyLastLibraryLocation];
+  [self.libraryComboBox setStringValue:self.currentLibraryLocation];
+
+  [self.unwarpedTracksOutputFolderTextField setStringValue:[self lastUnwarpedTracksOutputFolder]];
 }
 
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication {
   return YES;
 }
 
-
-- (NSString *)getiTunesLibraryLocation {
-  return self.currentLibraryLocation;
+- (void)setProgressMessage:(NSString *)message {
+  NSLog(@"%@", message);
+  [self.statusTextField setStringValue:message];
 }
+
 
 - (IBAction)browseLibraryLocation:(id)sender {
   NSOpenPanel *openPanel = [NSOpenPanel openPanel];
@@ -65,11 +100,62 @@
 }
 
 - (IBAction)browseUnwarpedTracksOutputFolder:(id)sender {
+  NSOpenPanel *openPanel = [NSOpenPanel openPanel];
+  openPanel.canChooseFiles = NO;
+  openPanel.canChooseDirectories = YES;
+  openPanel.allowsMultipleSelection = NO;
+  [openPanel runModal];
+  [self.unwarpedTracksOutputFolderTextField setStringValue:openPanel.filename];
+}
 
+- (NSInteger)searchForUnwarpedTracks:(NSString *)directory outputFile:(FILE *)outputFile numFilesFound:(NSInteger)numFilesFound {
+  NSFileManager *fileManager = [[[NSFileManager alloc] init] autorelease];
+  NSError *error;
+  NSArray *directoryContents = [fileManager contentsOfDirectoryAtPath:directory error:&error];
+  for(NSString *item in directoryContents) {
+    NSString *absoluteItemPath = [directory stringByAppendingFormat:@"/%@", item];
+    BOOL isDirectory = NO;
+    if([fileManager fileExistsAtPath:absoluteItemPath isDirectory:&isDirectory]) {
+      if(isDirectory) {
+        numFilesFound = [self searchForUnwarpedTracks:absoluteItemPath outputFile:outputFile numFilesFound:numFilesFound];
+      }
+      else {
+        NSRange lastDot = [absoluteItemPath rangeOfString:@"." options:NSBackwardsSearch];
+        if(lastDot.location != NSNotFound) {
+          NSString *extension = [absoluteItemPath substringFromIndex:lastDot.location + 1];
+          if([extension isEqualToString:@"asd"]) {
+            continue;
+          }
+          else if([self.recognizedAudioFileExtensions containsObject:[extension lowercaseString]]) {
+            NSString *asdPath = [absoluteItemPath stringByAppendingString:@".asd"];
+            if(![fileManager fileExistsAtPath:asdPath]) {
+              numFilesFound++;
+              if(numFilesFound == 1) {
+                [self setProgressMessage:[NSString stringWithFormat:@"%d file found", numFilesFound]];
+              }
+              else {
+                [self setProgressMessage:[NSString stringWithFormat:@"%d files found", numFilesFound]];
+              }
+              fprintf(outputFile, "%s\n", [absoluteItemPath cStringUsingEncoding:NSMacOSRomanStringEncoding]);
+            }
+          }
+        }
+      }
+    }
+  }
+  return numFilesFound;
 }
 
 - (IBAction)generateUnwarpedTracksPlaylist:(id)sender {
-
+  NSString *outputFilePath = [self.unwarpedTracksOutputFolderTextField.stringValue stringByAppendingString:kDefaultOutputPlaylistName];
+  NSString *topDirectory = [self.currentLibraryLocation stringByAppendingString:kiTunesTopLevelSubfolder];
+  FILE *outputFile = fopen([outputFilePath cStringUsingEncoding:NSASCIIStringEncoding], "w");
+  [self setProgressMessage:@"Search started..."];
+  [_progressIndicator startAnimation:sender];
+  NSInteger numFilesFound = 0;
+  [self searchForUnwarpedTracks:topDirectory outputFile:outputFile numFilesFound:numFilesFound];
+  [_progressIndicator stopAnimation:sender];
+  fclose(outputFile);
 }
 
 - (IBAction)findOrphanedAsds:(id)sender {
